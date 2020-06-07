883a59a0938598f92366b9b7b11ebe366b6c81fd
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2020-06-07 07:59:56 -0400

    added pypi stuff

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='automark',
+    version='0.1.1',
+    scripts=['automark', 'prepare', 'lms-download',
+             'lms-upload-marks', 'lms-upload-reports'],
+    author="Mustafa Quraish",
+    author_email="mustafa@cs.toronto.edu",
+    description="A highly configurable auto-marker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mustafaquraish/marker",
+    packages=setuptools.find_packages(),
+    py_modules=[
+        'testcases', 'utils', 'marksheet', 'config',
+        'lms'
+    ],
+    install_requires=[
+        'requests', 'pyyaml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
