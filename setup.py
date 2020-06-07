3e09bbd75a44663af3a3d2a0fc2ab4427860dad4
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2020-06-07 09:16:07 -0400

    change version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='automark',
-    version='0.1.1',
+    version='1.0.0',
     scripts=['automark', 'prepare', 'lms-download',
              'lms-upload-marks', 'lms-upload-reports',
              'lms-set-status'],
