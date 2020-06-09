f5cc43f8c2090c25c96590be661db5a49a001707
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2020-06-09 11:42:04 -0400

    don't need to provide an LMS for just marking now

--- marker/lms/__init__.py
+++ marker/lms/__init__.py
@@ -2,9 +2,14 @@ from .canvas import Canvas
 from .markus import Markus
 
 def LMS_Factory(config):
+    assert('lms' in config), "Must have an LMS in the config"
+    assert('base_url' in config), "Must have base url for LMS in config"
+    assert('assignment' in config), "Must have assignment ID in config"
+
     if config['lms'].lower() == 'canvas':
         assert('course' in config), "Must have course ID in Config for Canvas"
         assert('file_name' in config), "Must have name for downloaded file"
         return Canvas(config)
+    
     elif config['lms'].lower() == 'markus':
         return Markus(config)
