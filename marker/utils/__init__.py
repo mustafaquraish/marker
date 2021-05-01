4becb67cd27bd13783ce9509926b0e9a174d698d
Mustafa Quraish <mustafaquraish@gmail.com>
2021-05-01 18:22:41 -0400

    run_command now more robust and returns output

--- marker/utils/__init__.py
+++ marker/utils/__init__.py
@@ -2,10 +2,8 @@
 Just contains some general utilities needed for all the scripts.
 '''
 
-import os, signal
-import subprocess
+import os
 from contextlib import contextmanager
-
 from .marksheet import Marksheet
 
 @contextmanager
@@ -17,29 +15,41 @@ def pushd(new_dir):
     finally:
         os.chdir(previous_dir)
 
-def run_command(cmd, timeout=None, output=subprocess.DEVNULL):
+import subprocess as subproc
+import signal, shlex
+import time
+
+def run_command(cmd, timeout=None, output=True, limit=-1):
     '''
-    Run the command `cmd` in the current working directory, and return the 
-    status.The command is run for a maximum of `timeout` seconds, and the 
-    stdout/stderr are redirected to `output`.
+    Run the command `cmd` in the current working directory, and return a
+    tuple of:
+                    (return_code, output)
 
-    Returns: Either
-        ('exit', exit_status)
-            OR
-        ('timeout', None)
+    If `timeout` is specified, the process is killed after the given time,
+    and in this case the `return_code` is None.
+
+    By default both output is collected and returned, but this can be
+    disabled with `output=False`. If so, an empty string is returned.
     '''
-    if timeout is not None:
-        cmd = f"timeout {timeout} bash -c '{cmd}'"
+    command = shlex.split(cmd)
 
-    exit_status = subprocess.call(
-        cmd,
-        stdout=output,
-        stderr=output,
-        shell=True
-    )
+    stdout_fd = subproc.PIPE if output else subproc.DEVNULL
 
-    # The exit code from `timeout` is 124 if process times out
-    if exit_status == 124:
-        return ('timeout', None)
-    else:
-        return ('exit', exit_status)
+    try:
+        proc = subproc.Popen(command, 
+                             text=True,
+                             preexec_fn=os.setsid,
+                             stdout=stdout_fd,
+                             stderr=subproc.STDOUT
+                             )
+    except Exception as e:
+        # We have an unknown error here, possibly that the executable 
+        # file does not exist. Return -1 with the error message in stderr
+        return (-1, str(e))
+
+    try:
+        proc.wait(timeout=timeout)
+    except subproc.TimeoutExpired:
+        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+
+    return (proc.returncode, proc.stdout.read(limit) if output else "")
